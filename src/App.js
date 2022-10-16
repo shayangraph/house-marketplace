300e976b03a002938dd461892ca9e4e66d7fcc2f
Shayan Jafari <shayanja12345@gmail.com>
2022-10-16 12:10:28 +0330

    create house marketplace project and set firebase

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h3>hello world</h3>
+    </>
+  );
+};
+
+export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBfSfJU5j7KMcZvcxTv5PDygPn-Prtr9nc",
+  authDomain: "house-marketplace-app-85d89.firebaseapp.com",
+  projectId: "house-marketplace-app-85d89",
+  storageBucket: "house-marketplace-app-85d89.appspot.com",
+  messagingSenderId: "905468120994",
+  appId: "1:905468120994:web:487ccf09d9b7acd8e8acfb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
