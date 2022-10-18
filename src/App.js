b08f03d54171daa93dc7f81a0917c1dae0853317
shayan jafari <shayanja12345@gmail.com>
2022-10-18 12:27:14 +0330

    set react router dom and navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Explore from "./pages/Explore";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offer from "./pages/Offer";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 const App = () => {
   return (
     <>
-      <h3>hello world</h3>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offer />} />
+          <Route path="/profile" element={<Signin />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+        </Routes>
+        <Navbar />
+      </Router>
     </>
   );
 };
