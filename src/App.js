be885a4e7f7c3fc860622c6dcf063b1c5ff7330b
shayan jafari <shayanja12345@gmail.com>
2022-11-07 16:13:22 +0330

    create single page listing

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Profile from "./pages/Profile";
 import CreateListing from "./pages/CreateListing";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Listing from "./pages/Listing";
 
 const App = () => {
   return (
@@ -28,6 +29,10 @@ const App = () => {
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
         </Routes>
         <Navbar />
       </Router>
